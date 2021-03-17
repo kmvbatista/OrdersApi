47927b62fb1e8fe1c66b0b152c1d88b7215d9eb2
kennedybatista <kennedy.batista@ambevtech.com.br>
2021-03-17 11:57:54 -0300

    converting document value object on mapping

--- Infraestructure/Mappings/CustomerMapping.cs
+++ Infraestructure/Mappings/CustomerMapping.cs
@@ -1,4 +1,7 @@
+using System;
 using Domain.Entity;
+using Domain.Enums;
+using Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -9,7 +12,22 @@ namespace Infraestructure.Mappings
     public void Configure(EntityTypeBuilder<Customer> builder)
     {
       builder.Property(p => p.Name).HasColumnName(nameof(Customer.Name)).IsRequired();
-      builder.Property(p => p.Document.Value).HasColumnName(nameof(Customer.Document)).IsRequired();
+      builder.Property(p => p.Document.Value).HasConversion(stringStored => SetDocumentValue(stringStored), document => document.ToString())
+      .HasColumnName(nameof(Customer.Document)).IsRequired();
+    }
+
+    private Document SetDocumentValue(string value)
+    {
+      if (IsCpf(value))
+      {
+        return new Document(value: value, type: DocumentType.CPF);
+      }
+      return new Document(value: value, type: DocumentType.CNPJ);
+    }
+
+    private bool IsCpf(string value)
+    {
+      return value.Length == 11;
     }
   }
 }
