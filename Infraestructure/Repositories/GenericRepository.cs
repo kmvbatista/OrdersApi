b91e9b3f832e23debddf28bccc002c304141b5ca
kennedybatista <kennedy.batista@ambevtech.com.br>
2021-03-17 11:13:26 -0300

    feat: generic Repository

--- Infraestructure/Repositories/GenericRepository.cs
+++ Infraestructure/Repositories/GenericRepository.cs
@@ -1,8 +1,60 @@
+using Domain.Entity;
+using Domain.RepositoryContracts;
+using Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Infraestructure.Repositories
+namespace Infra.Repositories.GenericRepository
 {
-    public class GenericRepository
+  public abstract class GenericRepository<TEntity>
+      : IGenericRepository<TEntity> where TEntity : BaseEntity
+  {
+    public readonly MainContext _dbContext;
+
+    public GenericRepository(MainContext dbContext)
     {
+      _dbContext = dbContext;
     }
+
+    public async Task Create(TEntity entity)
+    {
+      await _dbContext.Set<TEntity>().AddAsync(entity);
+      await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Delete(Guid id)
+    {
+      var entity = await GetById(id);
+      _dbContext.Set<TEntity>().Remove(entity);
+      await _dbContext.SaveChangesAsync();
+    }
+
+    public virtual async Task<IList<TEntity>> GetAll()
+    {
+      return await _dbContext.Set<TEntity>().ToListAsync();
+    }
+
+    public virtual async Task<TEntity> GetById(Guid id)
+    {
+      return await _dbContext.Set<TEntity>()
+          .AsNoTracking()
+          .FirstOrDefaultAsync(e => e.Id == id);
+    }
+
+    public async Task Update(TEntity entity)
+    {
+      _dbContext.Set<TEntity>().Update(entity);
+      await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> ValidateEntityExistence(Guid entityId)
+    {
+      var entityExists = await _dbContext.Set<TEntity>().AnyAsync(x => x.Id == entityId);
+      if (!entityExists)
+        return false;
+      return true;
+    }
+  }
 }
