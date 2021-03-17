63766ac8e6bd67d87cbd5dc83e236ddf222ddba8
kennedybatista <kennedy.batista@ambevtech.com.br>
2021-03-17 11:33:16 -0300

    feat: Customer entity

---
+++ Domain/Entity/Customer.cs
@@ -0,0 +1,11 @@
+using System;
+using Domain.ValueObjects;
+
+namespace Domain.Entity
+{
+  public class Customer
+  {
+    public String Name { get; set; }
+    public Document Document { get; set; }
+  }
+}
