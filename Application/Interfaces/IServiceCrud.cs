352ef9624cd60712276c27fe34c4ca4ef49b8f7e
kennedybatista <kennedy.batista@ambevtech.com.br>
2021-03-24 00:42:23 -0300

    adding models mappings to IService crud

--- Application/Interfaces/IServiceCrud.cs
+++ Application/Interfaces/IServiceCrud.cs
@@ -1,16 +1,22 @@
 using System.Threading.Tasks;
 using System;
 using System.Collections.Generic;
+using Domain.Utils;
+using Domain.Models;
 
 namespace Application.Interfaces
 {
-  public interface IServiceCrud<TRequestModel, TResponseModel>
+  public interface IServiceCrud<TEntity, TRequestModel, TResponseModel>
+                                      where TEntity : Domain.Entity.BaseEntity
+                                    where TRequestModel : IRequestModel
+                                    where TResponseModel : IResponseModel
   {
     Task Create(TRequestModel request);
     Task Update(Guid id, TRequestModel request);
     Task Deactivate(Guid id);
     Task<TResponseModel> GetById(Guid id);
-    Task<IList<TResponseModel>> GetAll();
-    Task ValidateEntityExistence(Guid entityId);
+    Task<IEnumerable<TResponseModel>> GetAll();
+    TResponseModel MapCustomerToResponseModel(TEntity customer);
+    TEntity MapCustomerFromRequestModel(TRequestModel customer);
   }
 }
